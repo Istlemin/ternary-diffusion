f1fd9d1aefe099e2c16bc8e5276958b1291433ae
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2024-01-06 17:17:08 +0000

    initial commit

---
+++ evaluate.py
@@ -0,0 +1,113 @@
+import argparse
+
+import torch
+import torch.nn.functional as F
+
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    InterpolationMode,
+    Resize,
+    ToTensor,
+)
+
+from datasets import load_dataset
+from scheduler import DDIMScheduler
+from model import UNet
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+n_timesteps = 1000
+n_inference_timesteps = 50
+
+def eval(model,args):
+    augmentations = Compose([
+        Resize(args.resolution, interpolation=InterpolationMode.BILINEAR),
+        CenterCrop(args.resolution),
+        ToTensor(),
+    ])
+    
+     
+    def transforms(examples):
+        images = [
+            augmentations(image.convert("RGB"))
+            for image in examples["image"]
+        ]
+        return {"input": images}
+
+    dataset = load_dataset(
+        args.dataset_name,
+        args.dataset_config_name,
+        cache_dir=args.cache_dir,
+        split="train",
+    )
+    dataset.set_transform(transforms)
+
+    dataset_size = args.dataset_size or len(dataset)
+
+    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=dataset_size, shuffle=False)
+
+    batch = next(iter(train_dataloader))
+    all_dataset_images = batch["input"]
+    
+    noise_scheduler = DDIMScheduler(num_train_timesteps=n_timesteps,
+                                    beta_schedule="cosine")
+    
+    with torch.no_grad():
+        # has to be instantiated every time, because of reproducibility
+        generator = torch.manual_seed(0)
+        all_generated_images = []
+        for i in tqdm(range((dataset_size+args.eval_batch_size-1)//args.eval_batch_size)):
+            generated_images = noise_scheduler.generate(
+                model,
+                num_inference_steps=n_inference_timesteps,
+                generator=generator,
+                eta=0.5,
+                use_clipped_model_output=True,
+                batch_size=args.eval_batch_size,
+                output_type="numpy")
+
+            all_generated_images.append(generated_images["sample_pt"].cpu())
+        
+        all_generated_images = torch.cat(all_generated_images)[:dataset_size]
+
+    print(len(all_dataset_images))
+    print(len(all_generated_images))
+    
+    from torchmetrics.image.fid import FrechetInceptionDistance
+
+    fid = FrechetInceptionDistance(normalize=True)
+    fid.update(all_dataset_images, real=True)
+    fid.update(all_generated_images, real=False)
+
+    print(f"FID: {float(fid.compute())}")
+
+def main(args):
+    model = UNet(3, image_size=args.resolution, hidden_dims=[16, 32, 64, 128])
+
+    pretrained = torch.load(args.pretrained_model_path)["model_state"]
+    model.load_state_dict(pretrained, strict=False)
+
+    model = model.to(device)
+
+    eval(model, args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Simple script for image generation.")
+    parser.add_argument("--resolution", type=int, default=64)
+    parser.add_argument("--eval_batch_size", type=int, default=4)
+    parser.add_argument("--dataset_name", type=str, default=None)
+    parser.add_argument("--dataset_config_name", type=str, default=None)
+    parser.add_argument("--dataset_size", type=int, default=None)
+    parser.add_argument("--cache_dir", type=str, default=None)
+    parser.add_argument("--pretrained_model_path",
+                        type=str,
+                        default=None,
+                        help="Path to pretrained model")
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ quantization.py
@@ -0,0 +1,230 @@
+import copy
+import math
+import torch
+from torch import nn
+from typing import Optional, Tuple
+
+from model.unet import UNet
+
+NO_QUANT = {
+    "weight_quanter": None,
+    "act_quanter": None,
+}
+
+def clip_and_save(ctx, w, clip_val):
+    ctx.save_for_backward((w<-clip_val) | (w>clip_val))
+    return torch.clamp(w,-clip_val,clip_val)
+
+def gradient_apply_clipping(ctx, grad_output):
+    clip_mask, = ctx.saved_tensors
+    return grad_output * (~clip_mask)
+
+class TwnQuantizerFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, w : torch.Tensor, dim, clip_val):
+        w = clip_and_save(ctx, w, clip_val)
+
+        if dim is None:
+            dim = tuple(range(len(w.shape)))
+        if type(dim) is int:
+            dim = (dim,)
+        n = math.prod((w.shape[d]) for d in dim)
+
+
+        thres = torch.norm(w, p=1, dim=dim) / n * 0.7
+        for d in dim:
+            thres = thres.unsqueeze(d)
+
+        b = (w>thres).type(w.dtype) - (w<-thres).type(w.dtype)
+        alpha = torch.sum(torch.abs(b*w),dim=dim)/torch.sum(torch.abs(b),dim=dim)
+        for d in dim:
+            alpha = alpha.unsqueeze(d)
+
+        return alpha*b
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        Approximate the gradient wrt to the full-precision inputs
+        using the gradient wrt to the quantized inputs, 
+        zeroing out gradient for clipped values.
+        """
+        # Need to return one gradient for each argument,
+        # but we only want one for [w] 
+        return gradient_apply_clipping(ctx, grad_output), None, None
+
+class TwnQuantizer(nn.Module):
+    def __init__(self, clip_val=2.5):
+        super().__init__()
+        self.clip_val = clip_val
+
+    def forward(self,w, dim=None):
+        return TwnQuantizerFunction.apply(w,dim,self.clip_val)
+
+
+class MinMaxQuantizerFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, w : torch.Tensor, dim, bits, clip_val):
+        w = clip_and_save(ctx, w, clip_val)
+
+        if dim is None:
+            dim = tuple(range(len(w.shape)))
+        if type(dim) is int:
+            dim = (dim,)
+        
+        mn = mx = w
+        for d in dim:
+            mn = torch.min(mn,dim=d).values
+            mx = torch.max(mx,dim=d).values
+            mn = mn.unsqueeze(d)
+            mx = mx.unsqueeze(d)
+
+        alpha = (mx-mn + 1e-8)
+        size = (2**bits-1)
+        quant_w = torch.round((w-mn)/alpha*size)/size*alpha+mn
+
+        return quant_w
+
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        Approximate the gradient wrt to the full-precision inputs
+        using the gradient wrt to the quantized inputs, 
+        zeroing out gradient for clipped values.
+        """
+        # Need to return one gradient for each argument,
+        # but we only want one for [w] 
+        #print("back!!")
+        return gradient_apply_clipping(ctx, grad_output), None, None, None
+
+class MinMaxQuantizer(nn.Module):
+    def __init__(self, bits=8, clip_val=2.5):
+        super().__init__()
+        self.clip_val = clip_val
+        self.bits=bits
+
+    def forward(self,w, dim=None):
+        return MinMaxQuantizerFunction.apply(w,dim,self.bits,self.clip_val)
+
+
+class NoopQuantizer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,w,dim=None):
+        return w
+
+
+class QuantizedLinear(nn.Module):
+    def __init__(self, linear : nn.Linear, quant_config=NO_QUANT):
+        super().__init__()
+        
+        self.weight = linear.weight
+        self.bias = linear.bias
+        self.weight_quanter = quant_config["weight_quanter"]
+        self.act_quanter = quant_config["act_quanter"]
+
+    def forward(self, input, input_quantize_dim=None):
+        if self.act_quanter is not None:
+            input = self.act_quanter(input,input_quantize_dim)
+        quant_weight = self.weight_quanter(self.weight)
+        return nn.functional.linear(input, quant_weight, self.bias)
+    
+
+class QuantizedConv2d(nn.Module):
+    def __init__(self, conv2d : nn.Conv2d, quant_config=NO_QUANT):
+        super().__init__()
+        
+        self.weight = conv2d.weight
+        self.bias = conv2d.bias
+        self.stride = conv2d.stride
+        self.padding = conv2d.padding
+        self.dilation = conv2d.dilation
+        self.weight_quanter = quant_config["weight_quanter"]
+        self.act_quanter = quant_config["act_quanter"]
+
+    def forward(self, input, input_quantize_dim=None):
+        if self.act_quanter is not None:
+            input = self.act_quanter(input,input_quantize_dim)
+        quant_weight = self.weight_quanter(self.weight, (1,2,3))
+        return nn.functional.conv2d(input, quant_weight, self.bias,stride=self.stride, padding=self.padding, dilation=self.dilation)
+    
+def quantize_residual_block(block,quant_config):
+    block.conv1 = QuantizedConv2d(block.conv1,quant_config)
+    block.conv2 = QuantizedConv2d(block.conv2,quant_config)
+    if isinstance(block.residual_conv, nn.Conv2d):
+        block.residual_conv = QuantizedConv2d(block.residual_conv,quant_config)
+    
+
+def quantize_attention(attention,quant_config):
+    attention.to_qkv = QuantizedConv2d(attention.to_qkv,quant_config)
+    attention.to_out = QuantizedConv2d(attention.to_out,quant_config)
+
+def quantize_unet(model, quant_config=None):
+    model = copy.deepcopy(model)
+    if quant_config is None:
+        quant_config = {
+            "weight_quanter": MinMaxQuantizer(bits=4,clip_val=2.5),
+            "act_quanter": NoopQuantizer(),
+        }
+    
+    model.time_embedding[0] = model.time_embedding[0]
+    model.time_embedding[2] = model.time_embedding[2]
+    
+    # model.init_conv = QuantizedConv2d(model.init_conv,quant_config)
+    
+    for down_block in model.down_blocks:
+        quantize_residual_block(down_block[0],quant_config)
+        quantize_residual_block(down_block[1],quant_config)
+        if isinstance(down_block[2],nn.Identity):
+            down_block[3][1] = QuantizedConv2d(down_block[3][1],quant_config)
+        else:
+            quantize_attention(down_block[2].fn.fn,quant_config)
+            down_block[3] = QuantizedConv2d(down_block[3],quant_config)
+        
+    quantize_residual_block(model.mid_block1,quant_config)
+    quantize_attention(model.mid_attn.fn.fn,quant_config)
+    quantize_residual_block(model.mid_block2,quant_config)
+    
+    for up_block in model.up_blocks:
+        quantize_residual_block(up_block[0],quant_config)
+        quantize_residual_block(up_block[1],quant_config)
+        if isinstance(up_block[2],nn.Identity):
+            up_block[3][1] = QuantizedConv2d(up_block[3][1],quant_config)
+        else:
+            quantize_attention(up_block[2].fn.fn,quant_config)
+            up_block[3] = QuantizedConv2d(up_block[3],quant_config)
+            
+    quantize_residual_block(model.out_block,quant_config)
+    
+    # model.conv_out = QuantizedConv2d(model.conv_out,quant_config)
+    
+    quantized_param_names = [name+".weight" for name,module in list(model.named_modules()) if isinstance(module,QuantizedConv2d) or isinstance(module, QuantizedLinear)]
+    return model, quantized_param_names
+
+def calc_model_size(model, quantized_param_names):
+    quant_size = 0
+    other_size = 0 
+    others = []
+    for name, parameter in model.named_parameters():
+        if name in quantized_param_names:
+            quant_size += 2*parameter.numel()
+        else:
+            other_size += 16*parameter.numel()
+            others.append((parameter.numel(),name))
+    others.sort()
+    print(others[-5:])
+    print(f"Quant size: {quant_size/1000:.3f} kB")
+    print(f"Other size: {other_size/1000:.3f} kB")
+    print(f"Total size: {(quant_size+other_size)/1000:.3f} kB")
+    
+if __name__=="__main__":
+    model = UNet(3,
+                 image_size=64,
+                 hidden_dims=[16, 32, 64, 128],
+                 use_linear_attn=False)
+    
+    model, quantized_param_names = quantize_unet(model)
+    
+    calc_model_size(model,quantized_param_names)
